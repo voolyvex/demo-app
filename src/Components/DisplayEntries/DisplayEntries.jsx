a167f866d8673c30f520ae5932a4f071c39c59c7
voolyvex <1joeknapp@gmail.com>
2023-01-18 14:17:56 -0600

    Created DisplayEntries Component and Instantiated Component on App Component

---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,28 @@
+
+
+const DisplayEntries = (props) => {
+    return ( 
+        <table>
+        <thead>
+          <tr>
+            <th>Entry Number</th>
+            <th>Weight</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.parentEntries.map((entry, index) => {
+            return (
+              <tr>
+                <td>{index + 1}</td>
+                <td>{entry.weight}</td>
+                <td>{entry.date}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>        
+     );
+}
+ 
+export default DisplayEntries;
