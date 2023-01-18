ebeb7eef41dd6d6dd884c6e7dd998df72b7005ef
voolyvex <1joeknapp@gmail.com>
2023-01-18 15:52:14 -0600

    Complete Entry Chart Tracker Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
     <div>
       <DisplayEntries parentEntries={entries} />
       <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries} />
     </div>
   );
 }
